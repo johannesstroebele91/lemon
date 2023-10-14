288fa9d53cf7fcc5a557a17d5fe61da0bb8b207e
Luedeke <nils_luedeke@t-online.de>
2023-10-14 14:39:17 +0200

    feat: added HelloWorld API

--- server/src/main/java/com/example/demo/DemoApplication.java
+++ server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import api.HelloWorldController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = HelloWorldController.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
